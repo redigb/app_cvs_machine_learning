03d51b20f04a4f3bff556a637b2a24e040baec93
redigb <redrojofuego77foreve@gmail.com>
2025-05-31 10:11:24 -0500

    Iniciando proyecto ml CVS

---
+++ back_cvs/src/main/java/com/redrd/back_cvs/BackCvsApplication.java
@@ -0,0 +1,13 @@
+package com.redrd.back_cvs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackCvsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackCvsApplication.class, args);
+	}
+
+}
